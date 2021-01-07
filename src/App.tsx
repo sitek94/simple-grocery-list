cfaa63207f144d5a176246aeb2dc98b69e5afad0
sitek94 <msitkowski94@gmail.com>
2021-01-07 22:41:45 +0100

    new product is added to the grocery list

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,20 @@ import * as React from 'react';
 
 import GroceriesForm from './groceries-form';
 
+type Product = {
+  icon: string;
+  name: string;
+  amount: number;
+};
+
 function App() {
   const [groceries, setGroceries] = React.useState([
     { icon: '🥦', name: 'Broccoli', amount: 10 },
     { icon: '🥛', name: 'Milk', amount: 2 },
   ]);
 
-  const addProduct = (...args: any) => {
-    console.log(args);
+  const addProduct = (product: Product) => {
+    setGroceries(groceries.concat(product));
   };
 
   return (
